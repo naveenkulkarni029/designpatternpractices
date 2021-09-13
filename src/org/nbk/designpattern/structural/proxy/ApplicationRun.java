c36b5d234dc22987d1fc6f90676633e1584f94ac
Naveen B Kulkarni <naveenkulkarni029@gmail.com>
2021-09-13 16:32:54 +0530

    Update ApplicationRun.java

--- src/org/nbk/designpattern/structural/proxy/ApplicationRun.java
+++ src/org/nbk/designpattern/structural/proxy/ApplicationRun.java
@@ -1,1 +1,8 @@
+public class ApplicationRun {
+	
+	public static void main(String[] args) {
+		InternetConnection connection = new ProxyInternetConnection("naveen");
+		connection.grantConnection();
+	}
 
+}
